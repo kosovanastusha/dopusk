efd371381f8c9f54f6b56727122a2d05b67137ab
Rin <rin_777@list.ru>
2017-06-23 10:38:08 +0700

    Added function.c

---
+++ source/function.c
@@ -0,0 +1,29 @@
+#include "function.h"
+#include <math.h>
+
+int Proverka(float D)
+{
+	if (D == 0) {
+		return 0;
+	}
+	if (D < 0) {
+		return 1;
+	}
+	if (D > 0) {
+		return 2;
+	}
+	return 0;
+}
+
+float Discriment(float a,float b, float c)
+{
+	float D = b*b - 4 * a * c;
+	return D;
+}
+
+float Znachenie (float D, float a, float b, float c, int i)
+{
+	float x;
+	x = ((-1)* b + sqrt(D) * i) / (2 * a);
+	return x;
+}
